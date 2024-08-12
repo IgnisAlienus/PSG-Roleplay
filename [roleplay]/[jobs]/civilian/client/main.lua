afbf9a5b002b9b837c5c39afd1cacfc560cb28f9
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-11 19:46:24 -0500

    update: setspawn

--- [roleplay]/[jobs]/civilian/client/main.lua
+++ [roleplay]/[jobs]/civilian/client/main.lua
@@ -3,6 +3,10 @@ RegisterNetEvent('civilian:setInitialSpawn')
 AddEventHandler('civilian:setInitialSpawn', function(coords)
     print("Received event 'civilian:setInitialSpawn' with coordinates: ", coords.x, coords.y, coords.z)
     local playerPed = PlayerPedId()
+    
+    -- Add a delay before setting the coordinates
+    Citizen.Wait(1000)
+    
     SetEntityCoords(playerPed, coords.x, coords.y, coords.z, false, false, false, true)
     print("Player spawned at coordinates: ", coords.x, coords.y, coords.z)
 end)
