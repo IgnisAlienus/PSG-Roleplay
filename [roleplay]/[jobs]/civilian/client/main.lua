b5684d009b037023c83954e6fd8946cd55c5c248
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-11 19:32:41 -0500

    add: debug

--- [roleplay]/[jobs]/civilian/client/main.lua
+++ [roleplay]/[jobs]/civilian/client/main.lua
@@ -1,6 +1,8 @@
 -- resources/[roleplay]/[jobs]/civilian/client/main.lua
 RegisterNetEvent('civilian:setInitialSpawn')
 AddEventHandler('civilian:setInitialSpawn', function(coords)
+    print("Received event 'civilian:setInitialSpawn' with coordinates: ", coords.x, coords.y, coords.z)
     local playerPed = PlayerPedId()
     SetEntityCoords(playerPed, coords.x, coords.y, coords.z, false, false, false, true)
+    print("Player spawned at coordinates: ", coords.x, coords.y, coords.z)
 end)
--- [roleplay]/[jobs]/civilian/server/main.lua
+++ [roleplay]/[jobs]/civilian/server/main.lua
@@ -2,5 +2,7 @@
 AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
     local source = source
     local spawnCoords = vector3(441.84, -982.14, 30.69) -- Coordinates for the downtown police station in Los Santos
+    print("Player connecting: ", name, " with source: ", source)
+    print("Sending initial spawn coordinates: ", spawnCoords.x, spawnCoords.y, spawnCoords.z)
     TriggerClientEvent('civilian:setInitialSpawn', source, spawnCoords)
 end)
