42884de11acbdd56f495a77b061c0737c9c14b4b
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-12 18:02:14 -0500

    fixes ?

--- [roleplay]/[jobs]/civilian/server/main.lua
+++ [roleplay]/[jobs]/civilian/server/main.lua
@@ -1,29 +1,30 @@
--- server/main.lua
+-- civilian/server/main.lua
 
--- Hook into the playerConnecting event
 AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
     local source = source
     deferrals.defer()
 
     print(('[DEBUG] Player connecting: %s (source: %s)'):format(name, source))
 
-    -- Perform any necessary checks or initialization here
-    -- For example, check if the player has the required role
-    exports.discord_integration:CheckPlayerRole(source, function(hasRequiredRole)
+    -- Debugging: Check the type of `CheckPlayerRole` function reference
+    local checkPlayerRole = exports['discord_integration']:CheckPlayerRole
+    print('Type of checkPlayerRole:', type(checkPlayerRole))  -- This should print "function"
+
+    -- Call the `CheckPlayerRole` function
+    checkPlayerRole(source, function(hasRequiredRole)
         if type(hasRequiredRole) ~= 'boolean' then
             print(('[ERROR] Invalid type for hasRequiredRole: %s'):format(type(hasRequiredRole)))
             deferrals.done('An error occurred while checking your role.')
             return
         end
+
         if hasRequiredRole then
             print(('[DEBUG] Player %s (source: %s) has the required role.'):format(name, source))
+            deferrals.done()  -- Let the player connect
         else
             print(('[DEBUG] Player %s (source: %s) does not have the required role. Freezing player.'):format(name, source))
-            -- Trigger the client event to freeze the player
-            TriggerClientEvent('civilian:freezePlayer', source, true)
+            TriggerClientEvent('civilian:freezePlayer', source, true)  -- Freeze the player
+            deferrals.done('You do not have the required role in Discord to join this server.')
         end
-
-        deferrals.done()
-        print(('[DEBUG] Deferrals done for player: %s (source: %s)'):format(name, source))
     end)
 end)
