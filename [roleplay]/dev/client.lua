136b838bd8edf71b45140b7b8ea139d0f3571cbd
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-11 19:42:22 -0500

    update: coords

--- [roleplay]/dev/client.lua
+++ [roleplay]/dev/client.lua
@@ -2,5 +2,11 @@
 RegisterCommand('getcoords', function()
     local playerPed = PlayerPedId()
     local coords = GetEntityCoords(playerPed)
-    print(string.format("Current coordinates: x = %.2f, y = %.2f, z = %.2f", coords.x, coords.y, coords.z))
+    local msg = string.format("Current coordinates: x = %.2f, y = %.2f, z = %.2f", coords.x, coords.y, coords.z)
+    print(msg)
+    TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0},
+        multiline = true,
+        args = {"Me", msg}
+    })
 end, false)
