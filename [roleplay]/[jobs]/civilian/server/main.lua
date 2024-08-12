8183644342bf6d5561810be25a2caaa24fe8bb2c
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-12 18:03:10 -0500

    fixes ??

--- [roleplay]/[jobs]/civilian/server/main.lua
+++ [roleplay]/[jobs]/civilian/server/main.lua
@@ -4,7 +4,7 @@ AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
     local source = source
     deferrals.defer()
 
-    print(('[DEBUG] Player connecting: %s (source: %s)'):format(name, source))
+    print(string.format('[DEBUG] Player connecting: %s (source: %s)', name, source))
 
     -- Debugging: Check the type of `CheckPlayerRole` function reference
     local checkPlayerRole = exports['discord_integration']:CheckPlayerRole
@@ -13,16 +13,16 @@ AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
     -- Call the `CheckPlayerRole` function
     checkPlayerRole(source, function(hasRequiredRole)
         if type(hasRequiredRole) ~= 'boolean' then
-            print(('[ERROR] Invalid type for hasRequiredRole: %s'):format(type(hasRequiredRole)))
+            print(string.format('[ERROR] Invalid type for hasRequiredRole: %s', type(hasRequiredRole)))
             deferrals.done('An error occurred while checking your role.')
             return
         end
 
         if hasRequiredRole then
-            print(('[DEBUG] Player %s (source: %s) has the required role.'):format(name, source))
+            print(string.format('[DEBUG] Player %s (source: %s) has the required role.', name, source))
             deferrals.done()  -- Let the player connect
         else
-            print(('[DEBUG] Player %s (source: %s) does not have the required role. Freezing player.'):format(name, source))
+            print(string.format('[DEBUG] Player %s (source: %s) does not have the required role. Freezing player.', name, source))
             TriggerClientEvent('civilian:freezePlayer', source, true)  -- Freeze the player
             deferrals.done('You do not have the required role in Discord to join this server.')
         end
