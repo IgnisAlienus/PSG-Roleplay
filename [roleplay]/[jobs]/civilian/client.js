37ec4e1eba91edbf9b517cf08ec2fdaf5c34a697
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-17 15:00:04 -0500

    fix

--- [roleplay]/[jobs]/civilian/client.js
+++ [roleplay]/[jobs]/civilian/client.js
@@ -1,3 +1,6 @@
+// client.js
+
+// Event to receive role check result
 onNet('civilian:checkRoleResult', (hasRequiredRole) => {
   console.log(`[DEBUG] Received role check result: ${hasRequiredRole}`);
 
@@ -9,4 +12,30 @@ onNet('civilian:checkRoleResult', (hasRequiredRole) => {
   console.log(
     `[DEBUG] Player has been ${shouldFreeze ? 'frozen' : 'unfrozen'}.`
   );
+});
+
+// Hook into the player spawn event
+on('playerSpawned', () => {
+  const playerPed = PlayerPedId();
+  SetEntityCoords(playerPed, 425.1, -979.5, 30.7, 0, 0, 0, false);
+  SetEntityHeading(playerPed, 90.0);
+
+  // Request role check from the server
+  console.log(`[DEBUG] Emitting civilian:requestRoleCheck event`);
+  emitNet('civilian:requestRoleCheck');
+});
+
+// Overriding spawnmanager's default spawn behavior
+on('onClientMapStart', () => {
+  exports.spawnmanager.setAutoSpawn(false);
+  setTimeout(() => {
+    exports.spawnmanager.spawnPlayer({
+      x: 425.1,
+      y: -979.5,
+      z: 30.7,
+      heading: 90.0,
+      model: 'mp_m_freemode_01',
+      skipFade: false,
+    });
+  }, 1000);
 });
