973c40b2f746f0b4c287388971d02786ba838b02
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-20 18:39:22 -0500

    fix

--- [roleplay]/[core]/radio/fxmanifest.lua
+++ [roleplay]/[core]/radio/fxmanifest.lua
@@ -10,3 +10,11 @@ version '1.0.0'
 server_script 'server.js'
 
 client_script 'client.js'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+}
