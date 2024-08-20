a89bc83a661c2653de132b31c6f30a18b4d6d476
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-20 18:20:19 -0500

    update: admin cmds

--- [roleplay]/[core]/admin/client.js
+++ [roleplay]/[core]/admin/client.js
@@ -1,21 +1,5 @@
 // resources/admin/client.js
 // Handle giving the police weapon loadout
 onNet('admin:adminCommands', () => {
-  RegisterCommand(
-    'getcoords',
-    () => {
-      const playerPed = PlayerPedId();
-      const coords = GetEntityCoords(playerPed);
-      const msg = `Current coordinates: x = ${coords[0].toFixed(
-        2
-      )}, y = ${coords[1].toFixed(2)}, z = ${coords[2].toFixed(2)}`;
-      console.log(msg);
-      emit('chat:addMessage', {
-        color: [255, 0, 0],
-        multiline: true,
-        args: ['Me', msg],
-      });
-    },
-    false
-  );
+  console.log('Admin commands loaded.');
 });
