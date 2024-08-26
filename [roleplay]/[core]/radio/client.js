c9c6bb72c5bed951f0387dd7a2b3c5f2eb6c2c2d
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-26 18:49:44 -0500

    radio

--- [roleplay]/[core]/radio/client.js
+++ [roleplay]/[core]/radio/client.js
@@ -45,9 +45,21 @@ setTick(() => {
     NetworkSetTalkerProximity(0.0); // Set to 0.0 to transmit to all players in the channel
 
     console.log(`${playerName} is talking on channel ${currentChannel}`);
+    emit('chat:addMessage', {
+      args: [`You are now talking on channel ${currentChannel}`],
+    });
+
+    // Play start transmission sound
+    PlaySoundFrontend(-1, 'Start_Squelch', 'CB_RADIO_SFX', true);
   } else if (currentChannel !== null && IsControlJustReleased(0, 249)) {
     // Stop transmitting voice data
     NetworkClearVoiceChannel();
     console.log(`Stopped talking on channel ${currentChannel}`);
+    emit('chat:addMessage', {
+      args: [`You stopped talking on channel ${currentChannel}`],
+    });
+
+    // Play stop transmission sound
+    PlaySoundFrontend(-1, 'End_Squelch', 'CB_RADIO_SFX', true);
   }
 });
