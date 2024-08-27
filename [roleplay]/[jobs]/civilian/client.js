daa3bdc6c9c713d88ed98548f004ab3f02cb8571
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-08-27 17:06:45 -0500

    fix

--- [roleplay]/[jobs]/civilian/client.js
+++ [roleplay]/[jobs]/civilian/client.js
@@ -1,3 +1,6 @@
+let canVerify = true;
+let roleChecked = false;
+
 // Event to receive role check result
 onNet('civilian:checkRoleResult', (hasRequiredRole) => {
   console.log(`[DEBUG] Received role check result: ${hasRequiredRole}`);
@@ -53,4 +56,32 @@ on('__cfx_nui:verifyRoles', (data, cb) => {
     console.log(`[DEBUG] Re-verification attempted too soon.`);
     cb({ success: false });
   }
+});
+
+// Hook into the player spawn event
+on('playerSpawned', () => {
+  if (!roleChecked) {
+    const playerPed = PlayerPedId();
+    SetEntityCoords(playerPed, 425.1, -979.5, 29.9, 0, 0, 0, false);
+    SetEntityHeading(playerPed, 90.0);
+
+    // Request role check from the server
+    console.log(`[DEBUG] Emitting civilian:requestRoleCheck event`);
+    emitNet('civilian:requestRoleCheck');
+  }
+});
+
+// Overriding spawnmanager's default spawn behavior
+on('onClientMapStart', () => {
+  exports.spawnmanager.setAutoSpawn(false);
+  setTimeout(() => {
+    exports.spawnmanager.spawnPlayer({
+      x: 425.1,
+      y: -979.5,
+      z: 30.7,
+      heading: 90.0,
+      model: 'mp_m_freemode_01',
+      skipFade: false,
+    });
+  }, 1000);
 });
